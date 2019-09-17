5e6f8e04b3e6fa3f703e328bd1faf765a1593153
liujie <liujie26@baidu.com>
2019-09-17 18:43:44 +0800

    持续集成修改

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
-git push -f https://${access_token}@github.com/liujie2019/liujie2019.github.io.git master
+git push -f https://${travis_token}@github.com/liujie2019/liujie2019.github.io.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
